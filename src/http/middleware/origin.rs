d2733bc0f6ac33a17fc1d62f7d9fad2b6ca8c0b1
Igor Bartashevich <bartashevich_i@lwo.by>
2024-08-06 09:46:34 +0300

    feat: origin validation middleware

--- src/http/middleware/origin.rs
+++ src/http/middleware/origin.rs
@@ -1,17 +1,24 @@
-use crate::get_app_config;
-use crate::models::app::AppConfig;
-use hyper::{body::Incoming, service::Service, Request};
 use std::collections::HashSet;
 use std::sync::Arc;
+
+use bytes::Bytes;
+use futures::future::{BoxFuture, FutureExt};
+use http_body_util::combinators::BoxBody;
+use hyper::{body::Incoming, Request, Response, StatusCode};
+use hyper::Error;
+use hyper::service::Service;
+
+use crate::utils::hyper_util::empty;
 
 #[derive(Debug, Clone)]
 pub struct OriginValidation<S> {
     inner: S,
     client_ip: String,
-    trusted_origins: HashSet<String>,
+    trusted_origins: Arc<HashSet<String>>,
 }
+
 impl<S> OriginValidation<S> {
-    pub fn new(inner: S, client_ip: String, trusted_origins: Arc<_, _>) -> Self {
+    pub fn new(inner: S, client_ip: String, trusted_origins: Arc<HashSet<String>>) -> Self {
         OriginValidation {
             inner,
             client_ip,
@@ -19,22 +26,28 @@ impl<S> OriginValidation<S> {
         }
     }
 }
+
 type Req = Request<Incoming>;
 
 impl<S> Service<Req> for OriginValidation<S>
 where
-    S: Service<Req>,
+    S: Service<Req, Response = Response<BoxBody<Bytes, Error>>, Error = Error> + Send + 'static,
+    S::Future: Send + 'static,
 {
     type Response = S::Response;
     type Error = S::Error;
-    type Future = S::Future;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
     fn call(&self, req: Req) -> Self::Future {
-        for origin in self.trusted_origins {
-            println!("{}", origin);
+        let client_ip: String = self.client_ip.split(':').next().unwrap_or("").to_string();
+
+        if self.trusted_origins.contains(&client_ip) {
+            let fut = self.inner.call(req);
+            fut.boxed()
+        } else {
+            let mut resp = Response::new(empty());
+            *resp.status_mut() = StatusCode::UNAUTHORIZED;
+            futures::future::ok(resp).boxed()
         }
-
-        println!("{}", self.client_ip);
-
-        self.inner.call(req)
     }
 }
